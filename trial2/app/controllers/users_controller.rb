7035eb0240be4f67b6c58193cb7076f5ce5ccb9b
jroers <joe.roers@gmail.com>
2016-01-18 21:18:15 -0800

    Added create route/method

--- trial2/app/controllers/users_controller.rb
+++ trial2/app/controllers/users_controller.rb
@@ -4,8 +4,20 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.create(user_params)
+  	redirect_to root_path
   end
 
   def show
+  end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
