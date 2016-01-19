504625e08bfe027ef09bf15fe2e4ce14b925a25f
jroers <joe.roers@gmail.com>
2016-01-18 21:14:53 -0800

    Added index.html.erb info, added index method, updated routes

--- trial2/config/routes.rb
+++ trial2/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/show'
-
   root to: "users#index"
+
+  get "/users", to: 'users#index', as: "users"
+  get "/signup", to: 'users#new', as: "new_user"
+  get "/users/:id", to: 'users#show', as: "user"
+
 end
